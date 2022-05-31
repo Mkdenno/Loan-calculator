48c6e9bfedac6ec97ff70cae4a18d4da92e47c20
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-31 12:50:58 +0300

    It will add css to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ let loanAmount=document.getElementById("loan");
 let interestEl=document.getElementById("interest");
 let monthsToPay=document.getElementById("monthstopay");
 let monthEl;
+let eachMonth;
 let totalInterest;
+let totalLoan;
+let year;
+let months;
+let days;
 function check(){
     let loan=loanAmount.value;
     let interest=interestEl.value;
@@ -10,7 +15,9 @@ function check(){
     totalInterest=(loan*interest*monthPay)/100;
     console.log(totalInterest);
     monthEl=totalInterest + parseFloat(loan);
+    eachMonth=monthEl/12;
     console.log(monthEl);
+    document.getElementById("eachmonth").innerHTML=eachMonth;
     document.getElementById("month").innerHTML=monthEl;
 
 
